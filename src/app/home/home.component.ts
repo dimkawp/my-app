3cf94fe5613574ef0e6bf0518d6ecf3b30a4ce12
dimkawp <dimkawp@gmail.com>
2017-09-07 14:51:51 +0300

    add loader

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,8 +22,6 @@ export class Items {
   name: string;
 }
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -31,145 +29,193 @@ export class Items {
 })
  
 export class HomeComponent implements OnInit {
+
+private token = '3f01ed0ee987480c810586f4e9e22433';
+
 items: Items[] = [{ name: 'archie' }, { name: 'jake' }, { name: 'richard' }];
 
-apiUrlPremierLeague = 'http://api.football-data.org/v1/competitions/445/teams?timeFrame=n1';
+apiUrlPremierLeague   = 'http://api.football-data.org/v1/competitions/445/teams?timeFrame=n1';
 apiUrlPrimeraDivision = 'http://api.football-data.org/v1/competitions/455/teams';
+apiUrlLigaOne         = 'http://api.football-data.org/v1/competitions/450/teams';
 data: any = {};
 response = [];
 
-  teams: Teams[] = [
-    {
-        title: 'REAL MADRID',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/243.png',
-        rating: '5',
-        completed: false
-    },
-    {
-        title: 'JUVENTUS',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/45.png',
-        rating: '4',
-        completed: false
-    },
-    {
-        title: 'BAYERN MÜNCHEN',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/21.png',
-        rating: '5',
-        completed: false
-    },
-    {
-        title: 'CHELSEA',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/5.png',
-        rating: '5',
-        completed: false
-    },
-    {
-        title: 'MANCHESTER UNITED',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/11.png',
-        rating: '4',
-        completed: false
-    },
-    {
-        title: 'PARIS SAINT-GERMAIN',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/73.png',
-        rating: '4',
-        completed: false
-    },
-    {
-        title: 'BARCELONA',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/241.png',
-        rating: '5',
-        completed: false
-    },
-    {
-        title: 'ARSENAL',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/1.png',
-        rating: '4',
-        completed: false
-    },
-    {
-        title: 'ATLÉTICO MADRID',
-        img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/240.png',
-        rating: '4',
-        completed: false
-    },
-    {
-      title: 'MANCHESTER CITY',
-      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/10.png',
+teams: Teams[] = [
+  {
+      title: 'REAL MADRID',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/243.png',
+      rating: '5',
+      completed: false
+  },
+  {
+      title: 'JUVENTUS',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/45.png',
       rating: '4',
       completed: false
-    },
-    {
-      title: 'BORUSSIA DORTMUND',
-      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/22.png',
+  },
+  {
+      title: 'BAYERN MÜNCHEN',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/21.png',
+      rating: '5',
+      completed: false
+  },
+  {
+      title: 'CHELSEA',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/5.png',
+      rating: '5',
+      completed: false
+  },
+  {
+      title: 'MANCHESTER UNITED',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/11.png',
       rating: '4',
       completed: false
-    },
-    {
-      title: 'LIVERPOOL',
-      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/9.png',
+  },
+  {
+      title: 'PARIS SAINT-GERMAIN',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/73.png',
       rating: '4',
       completed: false
-    },
-    
-  ];
-  private token = '3f01ed0ee987480c810586f4e9e22433';
-  i = 0;
-  loader = 100;
-  team1 = [];
-  team2 = [];
-  resultsPremierLeague = [];
-  resultsPrimeraDivision = [];
-  IsVisible = true;
-  button_name ;
-  title = '';
+  },
+  {
+      title: 'BARCELONA',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/241.png',
+      rating: '5',
+      completed: false
+  },
+  {
+      title: 'ARSENAL',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/1.png',
+      rating: '4',
+      completed: false
+  },
+  {
+      title: 'ATLÉTICO MADRID',
+      img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/240.png',
+      rating: '4',
+      completed: false
+  },
+  {
+    title: 'MANCHESTER CITY',
+    img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/10.png',
+    rating: '4',
+    completed: false
+  },
+  {
+    title: 'BORUSSIA DORTMUND',
+    img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/22.png',
+    rating: '4',
+    completed: false
+  },
+  {
+    title: 'LIVERPOOL',
+    img: 'http://futhead.cursecdn.com/static/img/17/clubs_large/9.png',
+    rating: '4',
+    completed: false
+  },
+  
+];
+
+i = 0;
+loader = 100;
+team1 = [];
+team2 = [];
+resultsPremierLeague   = [];
+resultsPrimeraDivision = [];
+resultsLigaOne         = [];
+isClassLoader  = false;
+Complite       = false;
+RandomComplite = false;
+IsVisible      = true;
+button_name ;
+title = '';
+
   constructor(private http: Http) {}
   
-  ngOnInit() {
+//implements HomeComponent class 
+ngOnInit() {
 
-  }
+}
+//Test work with timeout  
+TestLoader(){
+  console.log('TestLoader');
+
+    setTimeout(()=>{
+      this.isClassLoader = true;
+      console.log('stop');
+    },2000);
+
+}
 //HTTP request fore football data
 PremierLeague(){
 
+  let myHeaders = new Headers();
+  myHeaders.append('X-Auth-Token', this.token); 
+
+  let options = new RequestOptions({ headers: myHeaders });
+      // Make the HTTP request:
+      this.http.get(this.apiUrlPremierLeague, options).subscribe(
+        (res: Response)=> {
+          const DataRequest = res.json();
+          this.resultsPremierLeague = DataRequest;
+          console.log('PremierLeague', DataRequest);
+
+          setTimeout(() => {
+            this.isClassLoader = true;
+            this.Complite = true;
+          },1000);
+        
+          this.isClassLoader = false;
+          this.Complite = false;
+        }
+      );
+}
+PrimeraDivision(){
+  
     let myHeaders = new Headers();
     myHeaders.append('X-Auth-Token', this.token); 
 
     let options = new RequestOptions({ headers: myHeaders });
         // Make the HTTP request:
-        this.http.get(this.apiUrlPremierLeague, options).subscribe(
+        this.http.get(this.apiUrlPrimeraDivision, options).subscribe(
           (res: Response)=> {
             const DataRequest = res.json();
-            this.resultsPremierLeague = DataRequest;
-            console.log('PremierLeague', DataRequest);
+            this.resultsPrimeraDivision = DataRequest;
+            console.log('PrimeraDivision', DataRequest);
+
+            setTimeout(() => {
+              this.isClassLoader = true;
+              this.Complite = true;
+            },1000);
+          
+            this.isClassLoader = false;
+            this.Complite = false;
           }
         );
   }
+LigaOne(){
+  
+    let myHeaders = new Headers();
+    myHeaders.append('X-Auth-Token', this.token); 
 
-  PrimeraDivision(){
-    
-        let myHeaders = new Headers();
-        myHeaders.append('X-Auth-Token', this.token); 
-    
-        let options = new RequestOptions({ headers: myHeaders });
-            // Make the HTTP request:
-            this.http.get(this.apiUrlPrimeraDivision, options).subscribe(
-              (res: Response)=> {
-                const DataRequest = res.json();
-                this.resultsPrimeraDivision = DataRequest;
-                console.log('PrimeraDivision', DataRequest);
-              }
-            );
-      }
+    let options = new RequestOptions({ headers: myHeaders });
+        // Make the HTTP request:
+        this.http.get(this.apiUrlLigaOne, options).subscribe(
+          (res: Response)=> {
+            const DataRequest = res.json();
+            this.resultsLigaOne = DataRequest;
+            console.log('LigaOne', DataRequest);
 
-  Test(){
-    console.log('Test');
-
-      setTimeout(()=>{
-        this.loader =this.loader + 10;
-        console.log('timeout');
-      },1000);
-
- }
+            setTimeout(() => {
+              this.isClassLoader = true;
+              this.Complite = true;
+            },1000);
+          
+            this.isClassLoader = false;
+            this.Complite = false;
+          }
+        );
+  }
 
   Close() {
     this.IsVisible = this.IsVisible ? false : true;
